ac49e2351d7d67bf2b74792511b23ed9410dfe34
Clive <tranvinhnhan.tech.clone@gmail.com>
2026-08-03 22:28:53 +0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length)
+            return false;
+
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.Length; i++)
+        {
+            frequency[s[i] - 'a']++;
+            frequency[t[i] - 'a']--;
+        }
+
+        foreach (int j in frequency)
+        {
+            if (j != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
